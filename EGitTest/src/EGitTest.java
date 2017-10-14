b733b1a84a8cda947e546d6382de919755fb863f
Manav Majumdar <kookygavinquack@gmail.com>
2017-10-14 17:15:57 -0400

    Manav -2

--- EGitTest/src/EGitTest.java
+++ EGitTest/src/EGitTest.java
@@ -4,6 +4,6 @@ public class EGitTest {
 	public static void main(String[] args)
 	{
 		System.out.println("OpenCV version: " + Core.VERSION);
-		System.out.println("Manav" + Core.VERSION);
+		System.out.println("Manav Msjumdar" + Core.VERSION);
 	}
 }
