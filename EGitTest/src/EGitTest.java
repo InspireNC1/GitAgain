70f7c5403f13924e6b0862211ab9c9e87b50d0ad
mjoy0210 <jmukherjee2002@gmail.com>
2017-10-14 17:22:11 -0400

    Joydeep's EGit change

--- EGitTest/src/EGitTest.java
+++ EGitTest/src/EGitTest.java
@@ -4,6 +4,6 @@ public class EGitTest {
 	public static void main(String[] args)
 	{
 		System.out.println("OpenCV version: " + Core.VERSION);
-		System.out.println("Manav Msjumdar" + Core.VERSION);
+		System.out.println("Joydeep Mukherjee: " + Core.VERSION);
 	}
 }
